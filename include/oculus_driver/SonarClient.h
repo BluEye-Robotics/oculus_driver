ee9371fac187d768f29636bf0e56b9f0168ffaac
Juan Pablo Pino Bravo <jp.pinob@gmail.com>
2023-10-21 16:42:41 +0200

    Fix format using clang-format

--- include/oculus_driver/SonarClient.h
+++ include/oculus_driver/SonarClient.h
@@ -1,17 +1,17 @@
 /******************************************************************************
  * oculus_driver driver library for Blueprint Subsea Oculus sonar.
  * Copyright (C) 2020 ENSTA-Bretagne
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -19,24 +19,22 @@
 #ifndef _OCULUS_DRIVER_SONAR_CLIENT_H_
 #define _OCULUS_DRIVER_SONAR_CLIENT_H_
 
-#include <iostream>
-#include <iomanip>
-#include <cstring>
+#include <oculus_driver/Oculus.h>
+#include <oculus_driver/OculusMessage.h>
+#include <oculus_driver/StatusListener.h>
+#include <oculus_driver/print_utils.h>
+#include <oculus_driver/utils.h>
+
+#include <boost/asio.hpp>
+#include <chrono>
 #include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
 #include <memory>
 #include <mutex>
 #include <thread>
-#include <chrono>
 #include <type_traits>
-
-#include <boost/asio.hpp>
-
-#include <oculus_driver/Oculus.h>
-#include <oculus_driver/utils.h>
-#include <oculus_driver/print_utils.h>
-#include <oculus_driver/StatusListener.h>
-
-#include <oculus_driver/OculusMessage.h>
 
 namespace oculus {
 
@@ -52,80 +50,78 @@ namespace oculus {
  * concurrently read and written to at the same time. The situation where a
  * protection is needed is concurrent write and creation/destruction on the
  * socket. Hence, the socket is only locked in the send(), close_connection()
- * and ???. 
+ * and ???.
  */
-class SonarClient
-{
-    public:
+class SonarClient {
+ public:
+  using IoService = boost::asio::io_service;
+  using IoServicePtr = std::shared_ptr<IoService>;
+  using Socket = boost::asio::ip::tcp::socket;
+  using SocketPtr = std::unique_ptr<Socket>;
+  using EndPoint = boost::asio::ip::tcp::endpoint;
+  using Duration = boost::posix_time::time_duration;
 
-    using IoService    = boost::asio::io_service;
-    using IoServicePtr = std::shared_ptr<IoService>;
-    using Socket       = boost::asio::ip::tcp::socket;
-    using SocketPtr    = std::unique_ptr<Socket>;
-    using EndPoint     = boost::asio::ip::tcp::endpoint;
-    using Duration     = boost::posix_time::time_duration;
+  enum ConnectionState { Initializing, Attempt, Connected, Lost };
 
-    enum ConnectionState { Initializing, Attempt, Connected, Lost };
+  using TimeSource = Message::TimeSource;
+  using TimePoint = Message::TimePoint;
 
-    using TimeSource = Message::TimeSource;
-    using TimePoint  = Message::TimePoint;
+ protected:
+  IoServicePtr ioService_;
+  SocketPtr socket_;
+  EndPoint remote_;
+  uint16_t sonarId_;
+  ConnectionState connectionState_;
+  mutable std::mutex socketMutex_;
 
-    protected:
-    
-    IoServicePtr       ioService_;
-    SocketPtr          socket_;
-    EndPoint           remote_;
-    uint16_t           sonarId_;
-    ConnectionState    connectionState_;
-    mutable std::mutex socketMutex_;
+  Duration checkerPeriod_;
+  boost::asio::deadline_timer checkerTimer_;
+  Clock clock_;
 
-    Duration                     checkerPeriod_;
-    boost::asio::deadline_timer  checkerTimer_;
-    Clock                        clock_;
-    
-    StatusListener statusListener_;
-    unsigned int   statusCallbackId_;
+  StatusListener statusListener_;
+  unsigned int statusCallbackId_;
 
-    Message::Ptr message_;
+  Message::Ptr message_;
 
-    // helper stubs
-    void checker_callback(const boost::system::error_code& err);
-    void check_reception(const boost::system::error_code& err);
+  // helper stubs
+  void checker_callback(const boost::system::error_code& err);
+  void check_reception(const boost::system::error_code& err);
 
-    public:
+ public:
+  SonarClient(const IoServicePtr& ioService,
+              const Duration& checkerPeriod = boost::posix_time::seconds(1));
 
-    SonarClient(const IoServicePtr& ioService,
-                const Duration& checkerPeriod = boost::posix_time::seconds(1));
+  bool is_valid(const OculusMessageHeader& header);
+  bool connected() const;
 
-    bool is_valid(const OculusMessageHeader& header);
-    bool connected() const;
+  size_t send(const boost::asio::streambuf& buffer) const;
 
-    size_t send(const boost::asio::streambuf& buffer) const;
+  // initialization states
+  void reset_connection();
+  void close_connection();
+  void on_first_status(const OculusStatusMsg& msg);
+  void connect_callback(const boost::system::error_code& err);
+  virtual void on_connect();
 
-    // initialization states
-    void reset_connection();
-    void close_connection();
-    void on_first_status(const OculusStatusMsg& msg);
-    void connect_callback(const boost::system::error_code& err);
-    virtual void on_connect();
-
-    // main loop begin
-    void initiate_receive();
-    void header_received_callback(const boost::system::error_code err,
-                                  std::size_t receivedByteCount);
-    void data_received_callback(const boost::system::error_code err,
+  // main loop begin
+  void initiate_receive();
+  void header_received_callback(const boost::system::error_code err,
                                 std::size_t receivedByteCount);
-    
-    // This is called regardless of the content of the message.
-    // To be reimplemented in a subclass (does nothing by default).
-    virtual void handle_message(const Message::ConstPtr& msg);
+  void data_received_callback(const boost::system::error_code err,
+                              std::size_t receivedByteCount);
 
-    template <typename TimeT = float>
-    TimeT time_since_last_message() const { return clock_.now<TimeT>(); }
+  // This is called regardless of the content of the message.
+  // To be reimplemented in a subclass (does nothing by default).
+  virtual void handle_message(const Message::ConstPtr& msg);
 
-    TimePoint last_header_stamp() const { return message_->timestamp(); }
+  template <typename TimeT = float>
+  TimeT time_since_last_message() const {
+    return clock_.now<TimeT>();
+  }
+
+  TimePoint last_header_stamp() const { return message_->timestamp(); }
 };
 
-} //namespace oculus
+}  // namespace oculus
 
-#endif //_OCULUS_DRIVER_SONAR_CLIENT_H_
+#endif  //_OCULUS_DRIVER_SONAR_CLIENT_H_
